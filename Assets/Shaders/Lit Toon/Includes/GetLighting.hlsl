1130f0fce92f58448affab96b78f7798bab018d7
Eusko <31598560+Eusko@users.noreply.github.com>
2021-09-02 14:59:06 -0400

    Initial commit

---
+++ Assets/Shaders/Lit Toon/Includes/GetLighting.hlsl
@@ -0,0 +1,52 @@
+//    Copyright (C) 2020 NedMakesGames
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float Attenuation) {
+#ifdef SHADERGRAPH_PREVIEW
+    Direction = half3(0.5, 0.5, -0.25);
+    Color = 1;
+    Attenuation = 1;
+#else
+#ifdef SHADOWS_SCREEN
+    half4 clipPos = TransformWorldToHClip(WorldPos);
+    half4 shadowCoord = ComputeScreenPos(clipPos);
+#else
+    half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+#endif
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    Attenuation = mainLight.distanceAttenuation * mainLight.shadowAttenuation;
+#endif
+}
+
+void AdditionalLight_float(float3 WorldPos, int Index, out float3 Direction,
+    out float3 Color, out float Attenuation)
+{
+    Direction = normalize(float3(0.5, 0.5, 0.25));
+    Color = float3(0.0f, 0.0f, 0.0f);
+    Attenuation = 0.0f;
+
+#ifndef SHADERGRAPH_PREVIEW
+    int pixelLightCount = GetAdditionalLightsCount();
+    if (Index < pixelLightCount) {
+        Light light = GetAdditionalLight(Index, WorldPos);
+        Direction = light.direction;
+        Color = light.color;
+        Attenuation = light.distanceAttenuation * light.shadowAttenuation;
+    }
+#endif
+}
